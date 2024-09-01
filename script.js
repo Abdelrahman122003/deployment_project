a8b4477d763179a1f1d39fe1a8bc7ad79a10ed2c
3bdelr7man <abdelrhmansayed122003@gmail.com>
2024-09-01 15:23:02 +0300

    front is connected to backend

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
-const path = require('path')
+const express = require("express");
+const axios = require("axios");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const path = require("path");
 
-app.use(express.json())
-app.set('view-engine', 'ejs')
+app.use(express.json());
+app.set("view-engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get('/' , (req , res)=>{
-    res.render('index.ejs');
-})
-
-
-app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+app.get("/front", async (req, res) => {
+  const users = (await axios.get("http://localhost:5001/users")).data;
+  res.json(users);
+});
+app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
